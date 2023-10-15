cb858a6f659bc5e8a8afff45745726d8c1420be8
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2023-10-15 22:06:11 +0300

    curve

--- app/page.js
+++ app/page.js
@@ -1,95 +1,75 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+    'use client'
+    import styles from './page.module.css'
+    import { useRef, useEffect } from 'react';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    export default function Home() {
+
+    const path = useRef(null);
+    let progress = 0;
+    let x = 0.5;
+    let time = Math.PI / 2;
+    let reqId = null;
+
+    useEffect(() => {
+        setPath(progress);
+    }, [])
+
+    const setPath = (progress) => {
+        const width = window.innerWidth * 0.7;
+        path.current.setAttributeNS(null, "d", `M0 250 Q${width * x} ${250 + progress}, ${width} 250`)
+    }
+
+    const lerp = (x, y, a) => x * (1 - a) + y * a
+
+    const manageMouseEnter = () => {
+        if(reqId){
+        cancelAnimationFrame(reqId)
+        resetAnimation()
+        }
+    }
+
+    const manageMouseMove = (e) => {
+        const { movementY, clientX } = e;
+        const pathBound =  path.current.getBoundingClientRect();
+        x = (clientX - pathBound.left) / pathBound.width;
+        progress+= movementY
+        setPath(progress);
+    }
+
+    const manageMouseLeave = () => {
+        animateOut();
+    }
+
+    const animateOut = () => {
+        const newProgress = progress * Math.sin(time);
+        progress = lerp(progress, 0, 0.025);
+        time+=0.2;
+        setPath(newProgress);
+        if(Math.abs(progress) > 0.75){
+        reqId = requestAnimationFrame(animateOut);
+        }
+        else{
+        resetAnimation();
+        }
+    }
+
+    const resetAnimation = () => {
+        time = Math.PI / 2;
+        progress = 0;
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.body}>
+                <div className={styles.line}>
+                <div onMouseEnter={() => {manageMouseEnter()}} onMouseMove={(e) => {manageMouseMove(e)}} onMouseLeave={() => {manageMouseLeave()}} className={styles.box}></div>
+                <svg>
+                    <path ref={path}></path>
+                </svg>
+                </div>
+
+                
+            </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    )
+    }
